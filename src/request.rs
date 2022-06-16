b21eabf42d3118a8e13aab2c192f8d029f2df879
Taha Elkarroumy <57259373+t-el@users.noreply.github.com>
2022-06-16 18:14:34 +0100

    Update request.rs

--- src/request.rs
+++ src/request.rs
@@ -1,21 +1,23 @@
 use std::collections::HashMap;
 use crate::parser::Args;
 use reqwest::{Method, header,RequestBuilder};
-use reqwest::header::HeaderMap;
-// what is the best anime ever 
+use reqwest::header::HeaderMap; 
 
 
 /// parse the header vector to a header map
-pub fn get_headers<>(headers: Vec<String>) -> Option <HeaderMap> {
+pub fn get_headers(headers: Vec<String>) -> Option<HeaderMap> {
     let mut header_map = HeaderMap::new();
-    let headers = headers.iter().map(|header| {
+    let headers = headers.iter().for_each(|header| {
         if !header.contains(":") {
             return ;
         }
         let header_split = header.split(":").collect::<Vec<&str>>();
         let key = header_split[0];
         let value = header_split[1];
-        header_map.insert(header::HeaderName::from_static(key), header::HeaderValue::from_static(value));
+        header_map.insert(
+            header::HeaderName::try_from(key).unwrap(),
+            header::HeaderValue::try_from(value).unwrap(),
+        );
     });
     Some(header_map)
 }
